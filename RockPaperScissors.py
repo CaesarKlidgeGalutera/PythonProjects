f34659c5d358d67e8c10237d80e579a4fd3dd1ba
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2023-03-05 10:56:45 +0800

    optimized the program

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -6,25 +6,24 @@ computer = random.choice(choices)
 player = input("Enter your choice (rock, paper, scissors): ")
 result = {"player": player, "computer": computer}
 
-
 def match(player_choice, computer_choice):
     output = ""
     if player == computer:
         output = "It's a tie!"
     elif player == "rock" and computer == "scissors":
-        output = "You win!"
+        output = "You win! Rock beats scissors"
     elif player == "rock" and computer == "paper":
-        output = "You lose!"
+        output = "You lose! Paper beats rock"
     elif player == "paper" and computer == "scissors":
-        output = "You lose!"
+        output = "You lose! Scissors cuts paper"
     elif player == "paper" and computer == "rock":
-        output = "You win!"
+        output = "You win! Paper beats rock"
     elif player == "scissors" and computer == "rock":
-        output = "You lose!"
+        output = "You lose! Rock beats scissors"
     elif player == "scissors" and computer == "paper":
-        output = "You win!"
+        output = "You win! Scissors cuts paper"
     return output
 
 
-print(result)
-print(match(player, computer))
+print("You chose " + result["player"] + ", computer chose " + result["computer"])
+print(match(result["player"], result["computer"]))
